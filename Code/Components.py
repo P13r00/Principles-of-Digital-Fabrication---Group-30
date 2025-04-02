b398d896f869bd3152e6d317163dd93f82652943
P13r00 <80446661+P13r00@users.noreply.github.com>
2025-04-02 15:54:14 +0300

    Update Components.py

--- Code/Components.py
+++ Code/Components.py
@@ -15,14 +15,14 @@ class Sensor:
 
 class Motor:
     def maxSpeedInTurn(int angle):
-        #calculates the maximum allowable speed in a turn, based on the angle
+        '''calculates the maximum allowable speed in a turn, based on the angle'''
         
     def driveForward(int time):
-        #move forward for time seconds
-        #stop if detects an obstacle???
+        '''move forward for time seconds
+        stop if detects an obstacle???'''
 
     def turn(int angle, bool direction)
-        #turn angle degrees either left (false), or right (true)
+        '''turn angle degrees either left (false), or right (true)'''
         
     def steer():
         pass

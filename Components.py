0e1a69980040a98f4ec34735a0586c89aeecc18c
P13r00 <80446661+P13r00@users.noreply.github.com>
2025-04-02 15:21:26 +0300

    Add files via upload

---
+++ Components.py
@@ -0,0 +1,15 @@
+class Decoder:
+    pass
+
+class Amplifier:
+    pass
+
+class Speaker:
+    pass
+
+class Sensor:
+    def isThereAnObstacle():
+        pass
+
+class Motor:
+    pass
---
+++ skel.py
@@ -0,0 +1,35 @@
+import time, asyncio
+import Components as com
+
+MAIN_SENSOR_TIME = 0.2
+x, y = 0, 0
+
+
+def initialiseMovement():
+    activateMotors()
+    activateSensor()
+
+def initialiseSound():
+    activateAmplifier()
+    activateDecoder()
+    activateSpeaker()
+    
+def moveRandomly():
+    pass
+
+async def move():
+    while True:
+        if not com.Sensor.isThereAnObstacle():
+            moveRandomly()
+        else:
+            steer()
+            
+        await asyncio.sleep(MAIN_SENSOR_TIME)
+
+
+def main():
+    #pre program pause
+    initialiseMovement()
+    initialiseSound()
+    playMp3()
+    asyncio.run(loop())

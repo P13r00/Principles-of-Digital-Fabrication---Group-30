bccc9ad95d155bd202da649f85abcad851cfd81c
P13r00 <80446661+P13r00@users.noreply.github.com>
2025-04-02 15:50:33 +0300

    playMp3 Code Sktetch

--- Code/skeleton.py
+++ Code/skeleton.py
@@ -1,7 +1,8 @@
-import time, asyncio
+import time, asyncio, pygame
 import Components as com
 
 MAIN_SENSOR_TIME = 0.2
+AUDIO_PATH = ""
 x, y = 0, 0
 
 
@@ -13,7 +14,17 @@ def initialiseSound():
     com.Amplifier.activateAmplifier()
     com.Decoder.activateDecoder()
     com.Speaker.activateSpeaker()
-    
+
+async def playMp3(): # ASYNC?????????
+    # https://raspberrypi.stackexchange.com/questions/7088/playing-audio-files-with-python
+    while True:
+        pygame.mixer.init()
+        pygame.mixer.music.load(AUDIO_PATH)
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy() == True:
+            continue
+        #stop when audio is over
+
 def moveRandomly():
     pass
 
@@ -23,14 +34,15 @@ async def move():
             com.Motor.moveRandomly()
         else:
             com.Motor.steer()
-            
         await asyncio.sleep(MAIN_SENSOR_TIME)
 
 def main():
     #pre program pause
     initialiseMovement()
     initialiseSound()
-    playMp3()
+
+    #CHECK ASYNCIO FUNCTIONALITY
+    asyncio.run(playMp3())
     asyncio.run(loop())
 
 if name == "main":

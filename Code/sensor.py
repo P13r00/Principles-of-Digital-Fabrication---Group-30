ae7f16a51b619c05f25ab6c61fd4f71c980aaf42
JHynynen <joonatan.hynynen@student.oulu.fi>
2025-04-16 14:27:57 +0300

    adding the simple sensor driver

---
+++ Code/sensor.py
@@ -0,0 +1,38 @@
+import time
+import board
+import digitalio
+
+trigPin = digitalio.DigitalInOut(board.GP11)
+trigPin.direction = digitalio.Direction.OUTPUT
+
+echoPin = digitalio.DigitalInOut(board.GP12)
+echoPin.direction = digitalio.Direction.INPUT
+
+microS = 0.000001
+
+distance = 0
+
+def main():
+    while True:
+        pulse()
+        print(distance + " cm")
+        time.sleep(0.5)
+
+def pulse():
+    trigPin.value = 0
+    time.sleep(5 * microS)
+    trigPin.value = 1
+    time.sleep(10 * microS)
+    trigpin.value = 0
+    
+    timer()
+
+def timer():
+    startTime = time.monotonic()
+    finalTime = 0
+    while finalTime == 0:
+        if echoPin.value == 1:
+            finalTime = time.monotonic()
+    duration = finalTime - startTime
+    cm = (duration/2) / 29.1
+    distance = cm

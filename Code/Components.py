c87937c0000f327fab533a42afec86d1a14bcb37
JHynynen <joonatan.hynynen@student.oulu.fi>
2025-04-16 14:48:22 +0300

    Update Components.py

--- Code/Components.py
+++ Code/Components.py
@@ -1,4 +1,7 @@
 import random as rand
+import time
+import board
+import digitalio
 
 class Decoder:
     pass
@@ -10,10 +13,48 @@ class Speaker:
     pass
 
 class Sensor:
-    def isThereAnObstacle():
-        pass
+    trigPin = digitalio.DigitalInOut(board.GP11)
+    trigPin.direction = digitalio.Direction.OUTPUT
+    
+    echoPin = digitalio.DigitalInOut(board.GP12)
+    echoPin.direction = digitalio.Direction.INPUT
+    
+    microS = 0.000001
+
+    def detect_obstacle():
+        distance = measure()
+        if (distance < 5):
+            return (True, distance)
+    
+    def measure():
+        distance = pulse()
+        return distance
+    
+    def pulse():
+        trigPin.value = 0
+        time.sleep(5 * microS)
+        trigPin.value = 1
+        time.sleep(10 * microS)
+        trigpin.value = 0
+        
+        return timer()
+    
+    def timer():
+        startTime = time.monotonic()
+        finalTime = 0
+        while finalTime == 0:
+            if echoPin.value == 1:
+                finalTime = time.monotonic()
+        duration = finalTime - startTime
+        cm = (duration/2) / 29.1
+        return cm
+
 
 class Motor:
+    def drive(float time):
+        
+
+class controller:
     def maxSpeedInTurn(int angle):
         '''calculates the maximum allowable speed in a turn, based on the angle'''
         

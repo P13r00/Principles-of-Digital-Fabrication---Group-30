1d299b636a2bcc3d44c658c5a48e94e3bfff4978
JHynynen <joonatan.hynynen@student.oulu.fi>
2025-04-23 13:34:21 +0300

    Update Components.py
    yeah... about the sensor class... yeah... it's in a better place now... yeah... it's called Sensor.py... yyeeeeaaahhhh... I'm so sorry...

--- Code/Components.py
+++ Code/Components.py
@@ -2,6 +2,8 @@ import random as rand
 import time
 import board
 import digitalio
+import Motor as mot
+import Sensor as sen
 
 class Decoder:
     pass
@@ -11,44 +13,6 @@ class Amplifier:
 
 class Speaker:
     pass
-
-class Sensor:
-    trigPin = digitalio.DigitalInOut(board.GP11)
-    trigPin.direction = digitalio.Direction.OUTPUT
-    
-    echoPin = digitalio.DigitalInOut(board.GP12)
-    echoPin.direction = digitalio.Direction.INPUT
-    
-    microS = 0.000001
-
-    def detect_obstacle():
-        distance = measure()
-        if (distance < 5):
-            return (True, distance)
-    
-    def measure():
-        distance = pulse()
-        return distance
-    
-    def pulse():
-        trigPin.value = 0
-        time.sleep(5 * microS)
-        trigPin.value = 1
-        time.sleep(10 * microS)
-        trigpin.value = 0
-        
-        return timer()
-    
-    def timer():
-        startTime = time.monotonic()
-        finalTime = 0
-        while finalTime == 0:
-            if echoPin.value == 1:
-                finalTime = time.monotonic()
-        duration = finalTime - startTime
-        cm = (duration/2) / 29.1
-        return cm
-        
 
 class controller:
     def maxSpeedInTurn(int angle):
